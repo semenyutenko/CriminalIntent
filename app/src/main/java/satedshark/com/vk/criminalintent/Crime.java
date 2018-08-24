c1da65088deb9cb4b37e7a1354340e9ff95bb6b0
semenyutenko <semenyutenko@gmail.com>
2018-08-25 02:04:39 +0300

    First commit.

---
+++ app/src/main/java/satedshark/com/vk/criminalintent/Crime.java
@@ -0,0 +1,47 @@
+package satedshark.com.vk.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    //region Fields
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    //endregion
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
