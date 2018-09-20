8dd75963f4678bbe907728eaf62f5f05eb6532d0
semenyutenko <semenyutenko@gmail.com>
2018-09-21 02:19:40 +0300

    Created a fragment library and added it to the main activity.

--- app/src/main/java/satedshark/com/vk/criminalintent/CrimeListFragment.java
+++ app/src/main/java/satedshark/com/vk/criminalintent/CrimeListFragment.java
@@ -1,16 +1,18 @@
 package satedshark.com.vk.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -30,16 +32,29 @@ public class CrimeListFragment extends Fragment {
 
     }
 
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI(){
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView mTitelTextView;
         private TextView mDataTextView;
+        private ImageView mSolvedImageView;
         Crime mCrime;
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent){
@@ -47,17 +62,19 @@ public class CrimeListFragment extends Fragment {
             itemView.setOnClickListener(this);
             mTitelTextView = itemView.findViewById(R.id.crime_title);
             mDataTextView = itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = itemView.findViewById(R.id.crime_solved);
         }
         public void bind(Crime crime){
             mCrime = crime;
             mTitelTextView.setText(crime.getTitle());
-            mDataTextView.setText(crime.getDate().toString());
+            mDataTextView.setText(DateFormat.format("EEEE, MMM d, yyyy",crime.getDate()));
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE:View.INVISIBLE);
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " was clicked", Toast.LENGTH_LONG )
-            .show();
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
