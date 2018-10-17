69258555f8bf66acaf20352ddef3c35548ae0d1f
semenyutenko <semenyutenko@gmail.com>
2018-10-18 00:01:46 +0300

    Created a fragment library and added it to the main activity.

--- app/src/main/java/satedshark/com/vk/criminalintent/CrimeFragment.java
+++ app/src/main/java/satedshark/com/vk/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -19,6 +20,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment{
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "dialog_date";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -67,7 +69,14 @@ public class CrimeFragment extends Fragment{
 
         mDateButton = v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.show(manager,DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
--- app/src/main/java/satedshark/com/vk/criminalintent/DatePickerFragment.java
+++ app/src/main/java/satedshark/com/vk/criminalintent/DatePickerFragment.java
@@ -1,7 +1,35 @@
 package satedshark.com.vk.criminalintent;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
 
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null).create();
+    }
 }
