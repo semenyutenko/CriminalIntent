b96a08d4ed6f1ea2ac7471d9677d2d5c7715cd4e
semenyutenko <semenyutenko@gmail.com>
2018-11-12 03:09:10 +0300

    sqlite updateCrime

--- app/src/main/java/satedshark/com/vk/criminalintent/CrimeLab.java
+++ app/src/main/java/satedshark/com/vk/criminalintent/CrimeLab.java
@@ -1,41 +1,68 @@
 package satedshark.com.vk.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import satedshark.com.vk.criminalintent.database.CrimeBaseHelper;
+import satedshark.com.vk.criminalintent.database.CrimeDbSchema;
+import satedshark.com.vk.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    private static ContentValues getContentValues (Crime crime){
+        ContentValues values = getContentValues(crime);
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved()? 1:0);
+        return values;
+    }
+
+    public void updateCrime(Crime crime){
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+        mDatabase.update(CrimeTable.NAME, values, CrimeTable.Cols.UUID + " =?",
+                new String[]{uuidString});
+    }
 
     public static CrimeLab get(Context context){
         if (sCrimeLab == null) sCrimeLab = new CrimeLab(context);
         return sCrimeLab;
     }
+
+
     private CrimeLab(Context context){
-        mCrimes = new ArrayList<>();
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public Crime getCrime(UUID id){
-        for(Crime crime:mCrimes){
-            if(crime.getId().equals(id)){
-                return crime;
-            }
-        }
+
         return null;
     }
 
+
+
     public void addCrime(Crime c){
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
     }
 
     public void delCrime(Crime c){
-        mCrimes.remove(c);
+
     }
 }
---
+++ app/src/main/java/satedshark/com/vk/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,61 @@
+package satedshark.com.vk.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+import satedshark.com.vk.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+
+    public CrimeBaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + CrimeTable.NAME +
+                "( _id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+                CrimeTable.Cols.SOLVED + ")");
+
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     *
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
